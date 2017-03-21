56a5d45d25bbb41403adab2e88809cf2a73e5ebf
John P <john.pickett@gmail.com>
2017-03-21 15:36:55 -0500

    ckp-21 complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+
+  get 'topics' => 'topics#index'
 
   get 'posts' => 'posts#index'
 
