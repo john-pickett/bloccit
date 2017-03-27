7969df9fa5c550bf29e0c49a878b234634fa13b6
John P <john.pickett@gmail.com>
2017-03-27 14:30:43 -0500

    asg-30 complete

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,6 +3,7 @@ class Vote < ActiveRecord::Base
   belongs_to :post
   after_save :update_post
 
+
   validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }, presence: true
 
   private
@@ -10,5 +11,5 @@ class Vote < ActiveRecord::Base
   def update_post
     post.update_rank
   end
-  
+
 end
