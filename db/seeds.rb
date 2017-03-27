d6a846c724b76671e66e7001e83ccc4f089c0c45
John P <john.pickett@gmail.com>
2017-03-27 11:45:34 -0500

    work in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,12 +21,14 @@ topics = Topic.all
 
  # Create Posts
  50.times do
-   Post.create!(
+   post = Post.create!(
    user: users.sample,
    topic: topics.sample,
    title:  RandomData.random_sentence,
    body:   RandomData.random_paragraph
    )
+   post.update_attribute(:created_at, rand(10.minutes .. 1.year).ago)
+   rand(1..5).times { post.votes.create!(value: [-1, 1].sample, user: users.sample) }
  end
  posts = Post.all
 
@@ -44,7 +46,8 @@ user = User.first
 user.update_attributes!(
   name: "john",
   email: 'john.pickett@gmail.com',
-  password: 'helloworld'
+  password: 'helloworld',
+  role: "admin"
 )
 
  puts "Seed finished"
@@ -52,3 +55,4 @@ user.update_attributes!(
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
+ puts "#{Vote.count} votes created"
