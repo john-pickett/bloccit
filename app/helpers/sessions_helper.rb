9bdd0edba04e87cd5fbaf5be1d55e5d3124b3c8e
John P <john.pickett@gmail.com>
2017-03-28 16:25:01 -0500

    asg complete pending tdd

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -11,4 +11,9 @@ module SessionsHelper
   def current_user
     User.find_by(id: session[:user_id])
   end
+
+  def post_author_pic(post)
+    author = User.find_by(id: post[:user_id])
+    return author.avatar_url(22)
+  end
 end
